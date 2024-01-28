1cd2549f5eea35d75208dcfe86a620e2438833eb
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2024-01-28 08:31:43 +0200

    class for basic crud statements set up

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+//Define database type
+if(!defined("DB_TYPE")){
+    define("DB_TYPE", "mysql");
+}
+
+//Define database host
+if(!defined("DB_HOST")){
+    define("DB_HOST", "localhost");
+}
+//Define database name
+if(!defined("DB_NAME")){
+    define("DB_NAME", "registration");
+}
+//Define database password
+if(!defined("DB_PWD")){
+    define("DB_PWD", "");
+}
+//Define database user
+if(!defined("DB_USER")){
+    define("DB_USER", "root");
+}
+
+?>
